3b0f06678aac4da49525af062ae9a5a583e55aad
brentoneill <brentoneill@gmail.com>
2018-01-04 20:34:54 -0700

    Cleaned up server code to es7

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+const express = require('express')
+const path = require('path')
+const app = express()
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'))
 
 // set the home page route
-app.get('*', function(req, res) {
-
+app.get('*', (req, res) => {
     // ejs render automatically looks in the views folder
-    res.render('index');
-});
+    res.render('index')
+})
 
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
-});
+app.listen(port, () => {
+    console.log(`Our app is running on http://localhost${port}`)
+})
