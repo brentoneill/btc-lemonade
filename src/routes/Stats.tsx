59d2f54d054b67b18de5422b9f852900fcf747b4
brentoneill <brentoneill@gmail.com>
2018-01-23 17:17:30 -0700

    Added recharts and stubbed stats page

--- src/routes/Stats.tsx
+++ src/routes/Stats.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { LineChart, Line, CartesianGrid, YAxis, XAxis } from 'recharts';
 
 interface IStatsProps {}
 
@@ -6,9 +7,25 @@ interface IStatsState {}
 
 export default class Stats extends React.Component<IStatsProps, IStatsState> {
     render() {
+        const data = [
+            { name: 'pageA', value: 400 },
+            { name: 'pageB', value: 300 },
+            { name: 'pageC', value: 300 },
+            { name: 'pageD', value: 200 },
+            { name: 'pageE', value: 350 },
+            { name: 'pageF', value: 100 }
+        ];
         return (
             <div className="Stats">
                Stats
+               <div>
+               <LineChart width={600} height={300} data={data}>
+                 <Line type="monotone" dataKey="value" stroke="#8884d8" />
+                 <CartesianGrid stroke="#ccc" />
+                 <XAxis dataKey="name" />
+                 <YAxis />
+                </LineChart>
+               </div>
             </div>
         );
     }
